06c6c579430648f5222ed915709470b204747952
AutomineStudio <h2andbenzo@gmail.com>
2025-08-25 16:37:46 +0100

    Update _app.js
    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,2 +1,25 @@
-import '../styles/globals.css';
-export default function MyApp({ Component, pageProps }) { return <Component {...pageProps} /> }
+// pages/_app.js
+import Script from "next/script";
+import "../styles/globals.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {/* Google Analytics (GA4) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-M8N1VDTDDQ"
+        strategy="afterInteractive"
+      />
+      <Script id="ga-gtag" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-M8N1VDTDDQ');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
